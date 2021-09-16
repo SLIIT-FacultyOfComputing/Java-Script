ab1356cead39129c61a07cc378e61813b9547532
it21076534Wickr <89467080+IT21076534@users.noreply.github.com>
2021-09-16 14:47:30 +0000

    Initial commit

---
+++ ext.js
@@ -0,0 +1,30 @@
+document.write('This is from External Javascript');
+document.write("<br>");
+document.write("<br>");
+document.write("4" / 3);
+document.write("<br>");
+document.write("5" + 5);
+document.write("<br>");
+document.write("5" - 3);
+document.write("<br>");
+document.write("5" * "5");
+document.write("<br>");
+document.write(4 * 3);
+document.write("<br>");
+document.write(5 * "5");
+document.write("<br>");
+
+var name = prompt('Enter marks');
+if (name => 50) {
+  document.write('pass');
+}
+else {
+  document.write('fail');
+}
+const number = prompt('Enter marks');
+if (name => 50) {
+  console.log('pass');
+}
+else {
+  console.log('fail');
+}
